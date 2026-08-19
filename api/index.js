30f7fabedb30d9ba648b8f083f85b1d952e15aed
Technoplankton <novikov666kherson@gmail.com>
2026-08-19 10:00:42 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
-const express = require('express');
 const https = require('https');
-const path = require('path');
 
-const app = express();
 const API_KEY = '3fd2be6f0cd0ed1635c61b9333f69e22';
-
-app.use(express.static(path.join(__dirname, '../public')));
 
 function getData(url) {
   return new Promise((resolve, reject) => {
@@ -23,27 +18,26 @@ function getData(url) {
   });
 }
 
-app.get('/api/search', async (req, res) => {
-  const query = req.query.q;
-  if (!query) return res.json({ results: [] });
+module.exports = async (req, res) => {
+  // Устанавливаем заголовки CORS и тип ответа
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+
+  const { action, q } = req.query;
 
   try {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ru-RU&query=${encodeURIComponent(query)}`;
-    const data = await getData(url);
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Ошибка сервера' });
-  }
-});
-
-app.get('/api/popular', async (req, res) => {
-  try {
+    if (action === 'search' && q) {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ru-RU&query=${encodeURIComponent(q)}`;
+      const data = await getData(url);
+      return res.status(200).json(data);
+    } 
+    
+    // По умолчанию отдаем популярное
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ru-RU&page=1`;
     const data = await getData(url);
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Ошибка сервера' });
-  }
-});
+    return res.status(200).json(data);
 
-module.exports = app;
+  } catch (err) {
+    return res.status(500).json({ error: 'Ошибка загрузки данных с сервера' });
+  }
+};
